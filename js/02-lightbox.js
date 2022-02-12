82f269fb5e222215d53723dee36665a45ff3b199
Natalie604917 <ni604917@gmail.com>
2022-02-12 13:08:38 +0200

    -

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// // Change code below this line
 
-console.log(galleryItems);
+
+const createPictureCard = ({ preview, original, description }) => {
+    return `
+<li>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+`;
+};
+
+const galleryMarkup = galleryItems.map(createPictureCard).join('');
+
+const cardsContainer = document.querySelector('.gallery');
+
+cardsContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
+
+
